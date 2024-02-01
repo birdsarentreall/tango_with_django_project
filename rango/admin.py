fa3bcae6b21e6a1a8c0ac6f16bcb60f9cdbc49e8
Michael Liang <2664748L@student.gla.ac.uk>
2024-02-01 15:35:27 +0000

    chapter 5, tests pass but incomplete

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    #tests pass, but urls dont appear : fix later
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
